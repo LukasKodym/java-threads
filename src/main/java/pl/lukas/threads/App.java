51d767e014f8a3a44b6dc4212a1e12d2bb42e9b0
LukasKodym <lukasz.kodym@gmail.com>
2019-04-09 18:26:42 +0200

    join method end 2.7

--- src/main/java/pl/lukas/threads/App.java
+++ src/main/java/pl/lukas/threads/App.java
@@ -4,26 +4,34 @@ import java.util.concurrent.TimeUnit;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("Główny wątek aplikacji " + Thread.currentThread().getName());
-        Thread thread = new MyThread("My-Thread-1");
-        thread.start();
 
-        Runnable runnable = () -> {
+        Runnable countdown = () -> {
             try {
-                System.out.println("Wykonywany wątek (runnable) "
+                System.out.println("Wykonywany wątek (countdown) "
                         + Thread.currentThread().getName());
-                for (int i = 0; i <= 10; i++) {
+                for (int i = 1; i <= 10; i++) {
                     System.out.println(i);
                     //Thread.sleep(1000); // the same as below
-                    TimeUnit.SECONDS.sleep(1);
+                    TimeUnit.MILLISECONDS.sleep( 100);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         };
-        Thread anotherThread = new Thread(runnable, "My-Runnable-1");
-        anotherThread.start();
+
+        Runnable blastOff = () -> {
+            System.out.println("Wykonywany wątek (blastOff) "
+                    + Thread.currentThread().getName());
+            System.out.println("Blast off");
+        };
+
+        Thread countdownThread = new Thread(countdown, "Countdowns");
+        Thread blastThread = new Thread(blastOff, "BlastOff");
+        countdownThread.start();
+        countdownThread.join(400);
+        System.out.println("Główny wątek aplikacji " + Thread.currentThread().getName());
+        blastThread.start();
     }
 }
