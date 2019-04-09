9e7a793b04ec2a5dd5e9a7f5d04ded96832faa3f
LukasKodym <lukasz.kodym@gmail.com>
2019-04-09 18:08:47 +0200

    sleep method in use

--- src/main/java/pl/lukas/threads/App.java
+++ src/main/java/pl/lukas/threads/App.java
@@ -5,14 +5,23 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("Główny wątek aplikacji " + Thread.currentThread().getName());
-
         Thread thread = new MyThread("My-Thread-1");
-
         thread.start();
 
-        Thread anotherThread = new Thread(() -> System.out.println("Wykonywany wątek (runnable) "
-                + Thread.currentThread().getName()), "My-Runnable-1");
+        Runnable runnable = () -> {
+            try {
+                System.out.println("Wykonywany wątek (runnable) "
+                        + Thread.currentThread().getName());
+                for (int i = 0; i <= 10; i++) {
+                    System.out.println(i);
 
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Thread anotherThread = new Thread(runnable, "My-Runnable-1");
         anotherThread.start();
     }
 }
