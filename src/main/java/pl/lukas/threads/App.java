4c91adcd864bc33f96233e003a4e018b7d9527ca
LukasKodym <lukasz.kodym@gmail.com>
2019-04-10 17:29:10 +0200

    update

--- src/main/java/pl/lukas/threads/App.java
+++ src/main/java/pl/lukas/threads/App.java
@@ -1,17 +1,23 @@
 package pl.lukas.threads;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class App {
 
     public static void main(String[] args) {
 
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
         Callable<Integer> answerToEverything = () -> {
             TimeUnit.SECONDS.sleep(10);
             return 42;
         };
 
+        executor.submit(answerToEverything);
 
+        executor.shutdown();
     }
 }
