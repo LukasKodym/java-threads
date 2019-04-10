aa45e6205846ab917fbc9f7336c30eeebc65fc7f
LukasKodym <lukasz.kodym@gmail.com>
2019-04-10 19:20:14 +0200

    exception catched end 2.18

--- src/main/java/pl/lukas/threads/App.java
+++ src/main/java/pl/lukas/threads/App.java
@@ -21,20 +21,27 @@ public class App {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            if(err){
+            if (err) {
                 throw new IllegalArgumentException("Wrong argument");
             }
             return 42;
-        }, executor).thenApply(bar -> {
-            System.out.println("*2 " + Thread.currentThread().getName());
-            return bar * 2;
-        }).thenApply(bar -> {
-            System.out.println("+1 " + Thread.currentThread().getName());
-            return bar + 1;
-        }).thenAccept(bar -> {
-            System.out.println("sout " + Thread.currentThread().getName());
-            System.out.println(bar);
-        });
+        }, executor)
+                .exceptionally(exception -> {
+                    System.out.println("ERROR " + exception.getMessage());
+                    return 2;
+                })
+                .thenApply(bar -> {
+                    System.out.println("*2 " + Thread.currentThread().getName());
+                    return bar * 2;
+                })
+                .thenApply(bar -> {
+                    System.out.println("+1 " + Thread.currentThread().getName());
+                    return bar + 1;
+                })
+                .thenAccept(bar -> {
+                    System.out.println("sout " + Thread.currentThread().getName());
+                    System.out.println(bar);
+                });
 
         executor.shutdown();
     }
