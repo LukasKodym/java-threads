3df957953eac0a14db51f2d1913e7679ddd77653
LukasKodym <lukasz.kodym@gmail.com>
2019-04-10 18:11:30 +0200

    executor as additional param in async methods end 2.15

--- src/main/java/pl/lukas/threads/App.java
+++ src/main/java/pl/lukas/threads/App.java
@@ -12,17 +12,20 @@ public class App {
         ExecutorService executor = Executors.newFixedThreadPool(5);
 
         CompletableFuture.runAsync(
-                () -> System.out.println("Wątek: " + Thread.currentThread().getName())
+                () -> System.out.println("Wątek: " + Thread.currentThread().getName()),
+                executor
         );
 
-        CompletableFuture<Integer> result = CompletableFuture.supplyAsync(() -> {
-            try {
-                TimeUnit.SECONDS.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return 42;
-        });
+        CompletableFuture<Integer> result = CompletableFuture.supplyAsync(
+                () -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return 42;
+                },
+                executor);
 
         System.out.println(result.get());
 
