347e039df67bf011420b2a2c074e8222313b6950
LukasKodym <lukasz.kodym@gmail.com>
2019-04-10 18:03:58 +0200

    CompletableFuture runAsync() method

--- src/main/java/pl/lukas/threads/App.java
+++ src/main/java/pl/lukas/threads/App.java
@@ -10,37 +10,26 @@ public class App {
 
         ExecutorService executor = Executors.newFixedThreadPool(5);
 
-        Callable<Integer> answerToEverything = () -> {
-            TimeUnit.SECONDS.sleep(10);
-            return 42;
-        };
+        CompletableFuture.runAsync(
+                () -> System.out.println("Wątek: " + Thread.currentThread().getName())
+        );
 
-        Callable<Integer> anotherAnswerToEverything = () -> {
-            TimeUnit.SECONDS.sleep(13);
-            return 43;
-        };
 
-        Callable<Integer> finalToEverything = () -> {
-            TimeUnit.SECONDS.sleep(5);
-            return 44;
-        };
+//        Callable<Integer> answerToEverything = () -> {
+//            TimeUnit.SECONDS.sleep(10);
+//            return 42;
+//        };
+//
+//        Callable<Integer> anotherAnswerToEverything = () -> {
+//            TimeUnit.SECONDS.sleep(13);
+//            return 43;
+//        };
+//
+//        Callable<Integer> finalToEverything = () -> {
+//            TimeUnit.SECONDS.sleep(5);
+//            return 44;
+//        };
 
-        List<Callable<Integer>> callableList = Arrays.asList(
-                answerToEverything,
-                anotherAnswerToEverything,
-                finalToEverything);
-
-        Future<Integer> result = executor.submit(answerToEverything);
-
-        List<Future<Integer>> futures = executor.invokeAll(callableList);
-
-        Integer bar = executor.invokeAny(callableList);
-
-        System.out.println(bar);
-
-//        for (Future<Integer> f : futures){
-//            System.out.println(f.get());
-//        }
 
         executor.shutdown();
     }
