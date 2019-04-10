54c05a76d3a836259269c2bc7ba8c28e17c2d3c0
LukasKodym <lukasz.kodym@gmail.com>
2019-04-10 17:23:38 +0200

    Callable

--- src/main/java/pl/lukas/threads/App.java
+++ src/main/java/pl/lukas/threads/App.java
@@ -1,48 +1,21 @@
 package pl.lukas.threads;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 public class App {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
-        System.out.println("Główny wątek aplikacji " + Thread.currentThread().getName());
+        Callable<Integer> answerToEverthing = new Callable<Integer>() {
 
-        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(2);
-
-        Runnable worker1 = () -> {
-            try {
-                System.out.println("Robotnik 1 - Aktualny wątek " + Thread.currentThread().getName());
-                System.out.println("Ładuje butlę z tlenem");
+            @Override
+            public Integer call() throws Exception {
                 TimeUnit.SECONDS.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                return 42;
             }
         };
 
-        Runnable worker2 = () -> {
-            try {
-                System.out.println("Robotnik 2 - Aktualny wątek " + Thread.currentThread().getName());
-                System.out.println("Ładuje zapas pożywienia");
-                TimeUnit.SECONDS.sleep(5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        };
-        Runnable worker3 = () -> {
-            try {
-                System.out.println("Robotnik 3 - Aktualny wątek " + Thread.currentThread().getName());
-                System.out.println("Ładuje paliwo");
-                TimeUnit.SECONDS.sleep(3);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        };
-
-        executorService.scheduleAtFixedRate(worker3,2,6,TimeUnit.SECONDS);
-//        executorService.shutdown();
     }
 }
